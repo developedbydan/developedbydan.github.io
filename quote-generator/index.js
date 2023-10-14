357c6076b43bf56c412c96c6f51963c947776ac2
Danijel <developedbydane@gmail.com>
2023-10-14 23:43:04 +0200

    Update
    Button active state

---
+++ quote-generator/index.js
@@ -0,0 +1,51 @@
+window.onload = function loadQuote() {
+    getQuote("https://type.fit/api/quotes?ref=hackernoon.com");
+
+    async function getQuote(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomQuote = data[randomIndex].text;
+        const authorString = data[randomIndex].author;
+
+        if(authorString == "type.fit"){
+            var randomAuthor = "Unknown"
+        }else{
+            const authorName = authorString.split(",");
+            var randomAuthor = authorName[0];
+        }
+
+        document.getElementById("quote").innerHTML = `“${randomQuote}”`; 
+        document.getElementById("author").innerHTML = randomAuthor;
+        }
+}
+
+
+
+
+
+function generateQuote(){
+    getQuote("https://type.fit/api/quotes?ref=hackernoon.com");
+
+    async function getQuote(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomQuote = data[randomIndex].text;
+        const authorString = data[randomIndex].author;
+
+        if(authorString == "type.fit"){
+            var randomAuthor = "Unknown"
+        }else{
+            const authorName = authorString.split(",");
+            var randomAuthor = authorName[0];
+        }
+
+        document.getElementById("quote").innerHTML = `“${randomQuote}”`; 
+        document.getElementById("author").innerHTML = randomAuthor;
+    }
+
+   
+}
+
+
